e16f10ccebbfdd81894122a758d92380c7312591
ZMRTZER <brdn.hamilton@gmail.com>
2019-03-12 13:15:01 -0600

    update

---
+++ Assignments/Lesson-10/script/weather.js
@@ -0,0 +1,34 @@
+//APPID = &APPID=a25b3602ce57e18b11c4ed2de661001a
+// City Codes = Preston "5604473", Fish Haven "5585010", Soda Springs "5607916"
+// URL = https://api.openweathermap.org/data/2.5/forecast?id=
+//URL Preston Full = https://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=a25b3602ce57e18b11c4ed2de661001a&units=imperial
+
+var apiKey = "&APPID=a25b3602ce57e18b11c4ed2de661001a";
+var cityCodes = {
+    "Preston" : "5604473",
+    "Fish Haven" : "5585010",
+    "Soda Springs" : "5607916"
+};
+
+function weatherInput(x) {
+    var state = document.getElementById("current-state");
+    var temp = document.getElementById("temperature");
+    var humid = document.getElementById("humidity");
+    var rain = document.getElementById("precipitation");
+    var speed = document.getElementById("speed");
+
+    var cityId = cityCodes[x];
+    var units= "&units=imperial";
+    var url = "https://api.openweathermap.org/data/2.5/forecast?id=";
+
+    var weatherRequest = new XMLHttpRequest();
+    weatherRequest.open("GET", url, true);
+    weatherRequest.send();
+    weatherRequest.onload = function() {
+        var  weatherData= JSON.parse(weatherRequest.responseText);
+        state = weatherData.weather.main;
+        temp = weatherData.main.temp;
+        humid = weatherData.
+        rain = weatherData.
+        speed = weatherData.
+}
--- Assignments/Lesson-10/script/weatherapi.js
+++ Assignments/Lesson-10/script/weatherapi.js
@@ -1,6 +1,27 @@
-const apiKey
-let weatherRequest = new XMLHttpRequest();
+//APPID = &APPID=a25b3602ce57e18b11c4ed2de661001a
+// City Codes = Preston "5604473", Fish Haven "5585010", Soda Springs "5607916"
+// URL = https://api.openweathermap.org/data/2.5/weather?id=
 
-weatherRequest.onload = function() {
-    let weatherData = JSON.parse(weatherRequest.responseText);
+
+var main = document.querySelector("span")
+var apiKey = "&APPID=a25b3602ce57e18b11c4ed2de661001a";
+var cityCodes = {
+    "Preston" : "5604473",
+    "Fish Haven" : "5585010",
+    "Soda Springs" : "5607916"
+};
+
+function setup() {
+    var span = document.getElementById("current-temp");
+    var url = "https://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=a25b3602ce57e18b11c4ed2de661001a&units=imperial";
+
+    var weatherRequest = new XMLHttpRequest();
+    weatherRequest.open("GET", url, true);
+    weatherRequest.send();
+    weatherRequest.onload = function() {
+        var  weatherData= JSON.parse(weatherRequest.responseText);
+        span.textContent = weatherData.main.temp;
+        console.log(weatherData);
+    }
+    main.appendChild();
 }
