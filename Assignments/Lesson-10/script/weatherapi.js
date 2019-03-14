0f0cd5e7f3fbdafc71a3827bbd6849acef65b608
ZMRTZER <brdn.hamilton@gmail.com>
2019-03-14 12:41:54 -0600

    update

--- Assignments/Lesson-10/script/weatherapi.js
+++ Assignments/Lesson-10/script/weatherapi.js
@@ -2,21 +2,12 @@
 // City Codes = Preston "5604473", Fish Haven "5585010", Soda Springs "5607916"
 // URL = https://api.openweathermap.org/data/2.5/weather?id=
 
-
-
-var apiKey = "&appid=a25b3602ce57e18b11c4ed2de661001a";
-var cityCodes = {
-    "Preston" : "5604473",
-    "Fish Haven" : "5585010",
-    "Soda Springs" : "5607916"
-};
-
 function setup() {
     var span = document.getElementById("current-temp");
     var url = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=a25b3602ce57e18b11c4ed2de661001a&units=imperial";
 
     var weatherRequest = new XMLHttpRequest();
-    weatherRequest.open("GET", url, true);
+    weatherRequest.open("GET", "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=a25b3602ce57e18b11c4ed2de661001a&units=imperial", true);
     weatherRequest.send();
     weatherRequest.onload = function() {
         var  weatherData= JSON.parse(weatherRequest.responseText);
