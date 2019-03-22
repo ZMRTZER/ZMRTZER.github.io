4d16d16314db7cd07dfb81686564ec3076f64fcb
ZMRTZER <brdn.hamilton@gmail.com>
2019-03-22 16:00:27 -0600

    update

--- Assignments/WeatherSite/script/sodaspringsweather.js
+++ Assignments/WeatherSite/script/sodaspringsweather.js
@@ -19,6 +19,37 @@ var weatherRequest = new XMLHttpRequest();
         var windChill = 35.74 + 0.6215 * weatherData.main.temp - 35.75 * Math.pow(weatherData.wind.speed, 0.16) + 0.4275 * weatherData.main.temp * Math.pow(weatherData.wind.speed, 0.16);
         windChill = Math.round(windChill);
         document.getElementById("wind").innerHTML = windChill;
+
+        var d = [weatherData.wind.deg];
+        var dir = "";
+
+        if ((d >= 337 && d <= 360) || (d >=0 && d <= 22)) {
+        dir ="N";
+        }
+        
+        else if (d >= 23 && d <= 67) {
+        dir ="NE";
+        }
+        else if (d >= 68 && d <= 112) {
+        dir ="E";
+        }
+        else if (d >= 113 && d <= 157) {
+        dir ="SE";
+        }
+        else if (d >= 158 && d <= 202) {
+        dir ="S";
+        }
+        else if (d >= 203 && d <= 246) {
+        dir ="SW";
+        }
+        else if (d >= 247 && d <= 290) {
+        dir ="W";
+        }
+        else dir = "NW"  
+
+        document.getElementById("direction").textContent=dir;
+
+
     }
 
 
