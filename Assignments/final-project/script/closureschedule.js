c6740ff4f2cfc378a04ab89115c925299efd55c3
ZMRTZER <brdn.hamilton@gmail.com>
2019-04-05 17:37:51 -0600

    update

--- Assignments/final-project/script/closureschedule.js
+++ Assignments/final-project/script/closureschedule.js
@@ -1,16 +1,17 @@
-var requestURL = 'https://zmrtzer.github.io/Assignments/final-project/json/templeschedule.json';
+var section = document.querySelector('section');
+var requestURL = 'https://raw.githubusercontent.com/ZMRTZER/ZMRTZER.github.io/master/Assignments/final-project/json/templeschedules.json';
 var request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.send();
 request.onload = function() {
-  var templeInfo = request.response;
-  showInfo(templeInfo);
+
 
     var closureData= JSON.parse(request.responseText);
     console.log(closureData);
 
-    document.getElementById("loganClosure").innerHTML = closureData.temples[1].closures;
-    document.getElementById("temperature").innerHTML = weatherData.main.temp;
-    document.getElementById("humidity").innerHTML = weatherData.main.humidity;
-    document.getElementById("speed").innerHTML = weatherData.wind.speed;
+    document.getElementById("loganClosure").innerHTML = closureData.temples[0].closures;
+    document.getElementById("atlantaClosure").innerHTML = closureData.temples[1].closures;
+    document.getElementById("ghanaClosure").innerHTML = closureData.temples[2].closures;
+    document.getElementById("saltlakeClosure").innerHTML = closureData.temples[3].closures;
+
 }
